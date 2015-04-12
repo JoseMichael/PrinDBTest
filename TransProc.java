9a71a0ec4c08e20113aee21caeeb8dd7b0899db3
pritswalke <priyankaw396@gmail.com>
2015-04-11 22:32:43 -0400

    TransProc.java
    Added a new class TransProc tohold the Transaction details.

---
+++ TransProc.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Priyanka
+ *
+ */
+public class TransProc {
+	
+	List<String> script=new ArrayList<>();
+	List<String> completedOperations=new ArrayList<>();
+	int index=-1;
+	int scriptNum;
+	boolean isWaiting=false;
+	
+}
