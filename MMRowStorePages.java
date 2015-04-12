7d524a2779beeb6f88efabd2b53bd11e62841803
JoseMichael <vegroth333@gmail.com>
2015-04-11 20:36:33 -0400

    Added Transaction Number to MMRowStorePages

--- MMRowStorePages.java
+++ MMRowStorePages.java
@@ -5,12 +5,19 @@ public class MMRowStorePages {
 	String Name[] = new String[16];
 	String PhoneNo[] = new String[16];
 	String TableName[] = new String[16];  //this is meta data
+	int TransactionNumber[] = new int[16]; //this is also meta data
+	
 	
 	int UsedForInserts = 0;
 	
 	public MMRowStorePages()
 	{
 		UsedForInserts = 0;
+		
+		for(int i=0; i<16; i++)
+		{
+			TransactionNumber[i]=-1;
+		}//initializes all transaction numbers to -1
 	}
 	
 	public boolean isEmpty()
