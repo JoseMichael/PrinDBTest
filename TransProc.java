28e7311ffe5de607524d329d3cce6d627bc805b2
pritswalke <priyankaw396@gmail.com>
2015-04-17 21:12:35 -0400

    Added method getNExtOperation in TransProc  class
    Added method getNExtOperation in TransProc  class

--- TransProc.java
+++ TransProc.java
@@ -13,5 +13,26 @@ public class TransProc {
 	int index=-1;
 	int scriptNum;
 	boolean isWaiting=false;
+	//TODO: GET FILESIZE
+	int filesize=-1;
+	
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	
+	public String getNextOperation() 
+	{
+		String operation="";
+		int index=getIndex();
+		if(index++<=script.size())
+		{
+			setIndex(index++);
+			operation=script.get(getIndex());
+		}
+		return operation;
+	}
 	
 }
